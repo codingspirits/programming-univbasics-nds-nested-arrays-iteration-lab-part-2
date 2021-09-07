684209150068e4584919494b0c6586a7f3564ab4
vincci-cheng-project-development-page <vincci.cheng.github@gmail.com>
2021-09-08 08:49:42 +1000

    Completed Assignment

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,17 @@
-def find_min_in_nested_arrays(src)
-  # src will be an array of arrays of integers
-  # Produce a new Array that contains the smallest number of each of the nested arrays
+sample = [[1,2,3],[4,5],[23,34]]
 
-end
+def find_min_in_nested_arrays(src)
+  outer_index = 0
+  while outer_index < src.length do
+    inner_index = 0
+    while inner_index < src[outer_index].length do
+      if src[outer_index][inner_index] === src[outer_index].min
+        p src[outer_index][inner_index]
+      end
+      inner_index +=1
+      end
+outer_index +=1
+    end
+  end
+  # Produce a new Array that contains the smallest number of each of the nested arrays
+find_min_in_nested_arrays sample
